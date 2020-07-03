531e84d17ee1f2d1885922dfa5420bcf33f2eb49
Maneesh <bhardwaj.maneesh@outlook.com>
2020-07-02 21:32:08 -0400

    First commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    public abstract class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+    class Adult : Person
+    {
+        public Adult(string fName, string lName)
+        {
+            this.FirstName = fName;
+            this.LastName = lName;
+        }
+    }
+    class Child : Person
+    {
+        public Child(string fName, string lName)
+        {
+            this.FirstName = fName;
+            this.LastName = lName;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person[] people = new Person[10];
+            people[0] = new Adult("Davis", "Kim");
+            people[1] = new Child("Tom", "Kim");
+
+            foreach (var item in people.Where(x => x != null))
+            {
+                Console.WriteLine($"{item.FirstName}  {item.LastName}");
+            }
+        }
+    }
+}
